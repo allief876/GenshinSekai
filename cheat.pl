e9c4b97c6545840ccdc801c1676e734ce38dbf2c
Allief Nuriman <57051116+allief876@users.noreply.github.com>
2020-11-22 09:08:00 +0700

    Penambahan cheat dan status:

---
+++ cheat.pl
@@ -0,0 +1,15 @@
+/* File: cheat.pl */
+/* NIM / Nama: 13519221 / Allief Nuriman */
+/* Serangan spesial, bisa dilakukan 3x sekali saja */
+
+/* cheat */
+/* Deklarasi Fakta */
+
+/* Deklarasi Rules */
+cheat :-
+    write('You cheating!'),
+    nl,
+    write('You deal'),
+    write(' X '),
+    write('damage'),
+    nl.
---
+++ status.pl
@@ -0,0 +1,26 @@
+/* File: status.pl */
+/* NIM / Nama: 13519221 / Allief Nuriman */
+/* Menampilkan status player */
+
+/* status */
+/* Deklarasi Fakta */
+
+
+/* Deklarasi Rules */
+status :-
+    write('Your status:'),
+    nl,
+    write('Job'),
+    nl,
+    write('Level: 1'),
+    nl,
+    write('Sanity: 1000/1000'),
+    nl,
+    write('Intelligence: 104'),
+    nl,
+    write('Defense: 89'),
+    nl,
+    write('Exp: 0/18'),
+    nl,
+    write('Gold: 1000'),
+    nl.
